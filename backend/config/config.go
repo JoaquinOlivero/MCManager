1adc915add03bc48ddd1aa62df68d96db5b1ec6f
JoaquinOlivero <joaquinolivero97@gmail.com>
2022-10-14 03:28:36 +0000

    added config file creation

--- backend/config/config.go
+++ backend/config/config.go
@@ -16,14 +16,28 @@ type Config struct {
 }
 
 func GetValues() Config {
+	var config Config
 
 	// Read config.json
 	content, err := os.ReadFile("./config.json")
 	if err != nil {
 		fmt.Println("Error when opening config file: ", err)
+
+		noConfigFile := os.IsNotExist(err)
+		if noConfigFile {
+			fmt.Println("Creating config.json file")
+			// Marshall config struct into json []byte
+			jsonConfig, _ := json.Marshal(config)
+			err := os.WriteFile("./config.json", jsonConfig, 0666)
+			if err != nil {
+				fmt.Println("Error when creating config file: ", err)
+			}
+			return config
+		}
+
 	}
+
 	// Unmarshall config content
-	var config Config
 	err = json.Unmarshal(content, &config)
 	if err != nil {
 		fmt.Println("Error during config.json Unmarshal(): ", err)
